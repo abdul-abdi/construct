1dd4ff775a2231dfdd4199b64195bbaf0836dc28
abdul-abdi <Abdullahiabdi1233@gmail.com>
2025-05-03 23:01:59 +0300

    Update next.config.ts to restrict image sources to Unsplash and enhance Content Security Policy

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**',
+        hostname: 'images.unsplash.com',
       },
     ],
     minimumCacheTTL: 60,
@@ -31,7 +31,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data:; font-src 'self' data:; connect-src 'self'",
+            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' blob: data: images.unsplash.com; font-src 'self' data:; connect-src 'self'",
           }
         ],
       },
